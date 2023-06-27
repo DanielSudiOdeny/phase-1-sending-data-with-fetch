8f598175fc80b01c9b112527d1e319be363874a0
Daniel Odeny <daniel.odeny@student.moringaschool.com>
2023-06-27 23:17:30 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Add your code here
+// const formData = { dogName: "Druski", dogBreed: "Poodle" };
+
+// const configurationObject = {
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json",
+//     Accept: "application/json",
+//   },
+//   body: JSON.stringify(formData),
+// };
+// fetch("http://localhost:3000/dogs", configurationObject)
+//   .then((response) => response.json())
+//   .then((data) => console.log(data))
+//   .catch((error) => {
+//     alert("Bad Things! Ragnarok!");
+//     console.log(error.message);
+//   });
+
+document.addEventListener("DOMContentLoaded", async function () {
+  const response = await submitData("Steve", "steve@steve.com");
+  return response;
+});
+
+function submitData(userName, userEmail) {
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({ name: userName, email: userEmail }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      document.body.innerHTML = `${data.id}`;
+    })
+    .catch((error) => {
+      document.body.innerHTML = `${error.message}`;
+    });
+}
